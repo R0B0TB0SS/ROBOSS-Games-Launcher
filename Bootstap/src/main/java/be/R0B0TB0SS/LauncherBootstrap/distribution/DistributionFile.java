e210e93b567cf5bfbcc30b3feea768a8642d0cbf
R0B0TB0SS <74618936+R0B0TB0SS@users.noreply.github.com>
2024-10-18 22:43:18 +0200

    Delete Bootstap directory

--- Bootstap/src/main/java/be/R0B0TB0SS/LauncherBootstrap/distribution/DistributionFile.java
+++
@@ -1,9 +0,0 @@
-package be.R0B0TB0SS.LauncherBootstrap.distribution;
-
-public class DistributionFile {
-    public String url;
-    public String sha1;
-
-    public DistributionFile() {
-    }
-}
--- Bootstap/src/main/java/be/R0B0TB0SS/LauncherBootstrap/utils/launcher/EnumProgress.java
+++
@@ -1,5 +0,0 @@
-package be.R0B0TB0SS.LauncherBootstrap.utils.launcher;
-
-public enum EnumProgress {
-    UPDATING, INITIALIZED;
-}
