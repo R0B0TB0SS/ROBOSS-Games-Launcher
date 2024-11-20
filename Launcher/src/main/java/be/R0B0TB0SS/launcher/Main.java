7e6bcc4fde7c751dc964b85b5230db5ebbfebe36
R0B0TB0SS <74618936+R0B0TB0SS@users.noreply.github.com>
2024-11-20 23:47:45 +0100

    20/11/24 23:47

--- Launcher/src/main/java/be/R0B0TB0SS/launcher/Main.java
+++ Launcher/src/main/java/be/R0B0TB0SS/launcher/Main.java
@@ -9,8 +9,8 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-                Class.forName("javafx.application.Application");
-                Application.launch(Launcher.class, args);
+            Class.forName("javafx.application.Application");
+            Application.launch(Launcher.class, args);
 
         } catch (ClassNotFoundException e) {
             JOptionPane.showMessageDialog(null, "Erreur:\n" + e.getMessage() + " not found", "Erreur", JOptionPane.ERROR_MESSAGE);
